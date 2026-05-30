f94625057698072225c78b41071d9443f40ebcce
facilitytrackergms-hub <facilitytracker.gms@gmail.com>
2026-05-30 19:59:21 -0400

    Create supabaseClient.js

---
+++ js/supabaseClient.js
@@ -0,0 +1,8 @@
+// supabaseClient.js
+
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+const SUPABASE_URL = 'https://uqrgjmzptliursudexbx.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_YdowS2hJJlYITNEHEIQpag_tgYk6f7P';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
