091b46134c663d104c8ffb6ba570e2e5c360521f
facilitytrackergms-hub <facilitytracker.gms@gmail.com>
2026-06-01 22:37:05 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 // main.js - Navigation and App Entry
 
-case 'view3_contacts': renderContactsDashboard(payload.facility); break;
-case 'view4_projects': renderPendingProjects(payload.facility); break;
-case 'view5_issues': renderIndividualConcerns(payload.facility); break;
-case 'view6_images': renderFacilityImages(payload.facility); break;
-case 'view7_followups': renderIssueFollowups(payload.issue); break;
-case 'view8_reports': renderReports(payload.facility); break;
+import { renderFacilitiesDashboard } from '../views/view_1_facility/view_1_grid.js';
+import { renderFacilityControls } from '../views/view_2_controls/view_2_grid.js';
+import { renderContactsDashboard } from '../views/view_3_controls/view_3_grid.js';
+import { renderPendingProjects } from '../views/view_4_projects/view_4_grid.js';
+import { renderIndividualConcerns } from '../views/view_5_issues/view_5_grid.js';
+import { renderFacilityImages } from '../views/view_6_images/view_6_grid.js';
+import { renderIssueFollowups } from '../views/view_7_followups/view_7_grid.js';
+import { renderReports } from '../views/view_8_report/view_8_grid.js';
 
 window.navigateTo = (viewName, payload = {}) => {
     switch (viewName) {
@@ -19,13 +21,48 @@ window.navigateTo = (viewName, payload = {}) => {
             }
             renderFacilityControls(payload.facility);
             break;
-        // Future views:
-        // case 'view3_contacts': renderContactsDashboard(payload.facility); break;
-        // case 'view4_projects': renderPendingProjects(payload.facility); break;
-        // case 'view5_issues': renderIndividualConcerns(payload.facility); break;
-        // case 'view6_images': renderFacilityImages(payload.facility); break;
-        // case 'view7_followups': renderIssueFollowups(payload.issue); break;
-        // case 'view8_reports': renderReports(payload.facility); break;
+        case 'view3_contacts':
+            if (!payload.facility) {
+                alert('Facility context missing!');
+                return;
+            }
+            renderContactsDashboard(payload.facility);
+            break;
+        case 'view4_projects':
+            if (!payload.facility) {
+                alert('Facility context missing!');
+                return;
+            }
+            renderPendingProjects(payload.facility);
+            break;
+        case 'view5_issues':
+            if (!payload.facility) {
+                alert('Facility context missing!');
+                return;
+            }
+            renderIndividualConcerns(payload.facility);
+            break;
+        case 'view6_images':
+            if (!payload.facility) {
+                alert('Facility context missing!');
+                return;
+            }
+            renderFacilityImages(payload.facility);
+            break;
+        case 'view7_followups':
+            if (!payload.issue) {
+                alert('Issue context missing!');
+                return;
+            }
+            renderIssueFollowups(payload.issue);
+            break;
+        case 'view8_reports':
+            if (!payload.facility) {
+                alert('Facility context missing!');
+                return;
+            }
+            renderReports(payload.facility);
+            break;
         default:
             console.warn('Unknown view:', viewName);
             break;
