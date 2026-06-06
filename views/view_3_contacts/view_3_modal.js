f58c329c8d7f458451eee5761263a686af813416
facilitytrackergms-hub <facilitytracker.gms@gmail.com>
2026-06-06 09:37:25 +0000

    Update view_3_modal.js

--- views/view_3_contacts/view_3_modal.js
+++ views/view_3_contacts/view_3_modal.js
@@ -6,6 +6,7 @@ STRICT HEADER RULE:
 Do not ever remove or change this header section.
 Always keep the header at the top of current files and new files.
 ================================================= */
+const __FILENAME = 'view_3_modal.js';
 import { insertContact, updateContact } from './view_3_data.js';
 import { supabase } from '../../js/supabaseClient.js';
 
