62ef19ec52c821330656b77679c83a1203a8171e
facilitytrackergms-hub <facilitytracker.gms@gmail.com>
2026-06-06 12:19:34 +0000

    Update view_4_modal.js

--- views/view_4_projects/view_4_modal.js
+++ views/view_4_projects/view_4_modal.js
@@ -6,6 +6,9 @@ STRICT HEADER RULE:
 Do not ever remove or change this header section.
 Always keep the header at the top of current files and new files.
 ================================================= */
+const __FILENAME = 'view_4_modal.js';
+
+
 import { insertFacilityProject } from './view_4_data.js';
 
 export function setupProjectsEvents(facility, renderPendingProjectsFn) {
