6f32e215de41a919e003e8bba99e410c354f7359
facilitytrackergms-hub <facilitytracker.gms@gmail.com>
2026-06-06 12:23:31 +0000

    Update view_6_grid.js

--- views/view_6_images/view_6_grid.js
+++ views/view_6_images/view_6_grid.js
@@ -6,6 +6,8 @@ STRICT HEADER RULE:
 Do not ever remove or change this header section.
 Always keep the header at the top of current files and new files.
 ================================================= */
+const __FILENAME = 'view_6_grid.js';
+
 import { setupGalleryEvents } from './view_6_modal.js';
 import { supabase } from '../../js/supabaseClient.js';
 
