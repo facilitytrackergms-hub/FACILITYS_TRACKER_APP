f7428474940c6d932f344d1cf1b8fe0a978a3634
facilitytrackergms-hub <facilitytracker.gms@gmail.com>
2026-06-06 12:24:37 +0000

    Update view_6_modal.js

--- views/view_6_images/view_6_modal.js
+++ views/view_6_images/view_6_modal.js
@@ -6,6 +6,8 @@ STRICT HEADER RULE:
 Do not ever remove or change this header section.
 Always keep the header at the top of current files and new files.
 ================================================= */
+const __FILENAME = 'view_6_modal.js';
+
 import { renderImageManagerSection } from '../../js/imageManager.js';
 
 export function setupGalleryEvents(facility) {
