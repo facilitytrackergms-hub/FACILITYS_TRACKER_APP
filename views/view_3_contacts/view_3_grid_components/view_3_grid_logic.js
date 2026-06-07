f4376a7ad7a06826669562709ae9ef847c5e671c
facilitytrackergms-hub <facilitytracker.gms@gmail.com>
2026-06-07 01:13:16 +0000

    Update view_3_grid_logic.js

--- views/view_3_contacts/view_3_grid_components/view_3_grid_logic.js
+++ views/view_3_contacts/view_3_grid_components/view_3_grid_logic.js
@@ -43,10 +43,9 @@ FILE NAME    : view_3_grid_logic.js
 SUPABASE TBL : contacts
 VIEW NAME    : Facility Directory Logic
 POP-UP TITLE : Manage Directory Entries
-LAST UPDATED : 2026-06-06 @ 09:09 PM
+LAST UPDATED : 2026-06-06 @ 09:12 PM
 ================================================================*/
-// FIXED: Adjusted path structures outward to target the root directory layout since execution occurs inside a sub-component pathway
-import { fetchContacts, createContact, updateContact, deleteContact } from '../view_3_data.js';
+import { fetchContacts, createContact, updateContact, deleteContact } from './view_3_data.js';
 import { attachModalStampTracker } from '../view_3_modal.js';
 import { setupContactsEvents } from './view_3_grid_logic.js';
 
