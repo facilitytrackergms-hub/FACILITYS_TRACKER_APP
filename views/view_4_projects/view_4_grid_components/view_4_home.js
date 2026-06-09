60b846ff2f43994d3f3c2266a0c94e83c3de1177
facilitytrackergms-hub <facilitytracker.gms@gmail.com>
2026-06-09 19:57:17 -0400

    Update view_4_home.js

--- views/view_4_projects/view_4_grid_components/view_4_home.js
+++ views/view_4_projects/view_4_grid_components/view_4_home.js
@@ -22,7 +22,7 @@ import {
     escapeHtml,
     renderProjectButtons,
     renderHomeModals
-} from './view_4_grid_components/view_4_render_helpers.js';
+} from './view_4_render_helpers.js';
 
 import {
     renderStyles
