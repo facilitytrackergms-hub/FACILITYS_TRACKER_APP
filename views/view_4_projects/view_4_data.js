fc25a3086e20944f876a7376b828215d290b3354
facilitytrackergms-hub <facilitytracker.gms@gmail.com>
2026-06-10 03:56:04 -0400

    Update view_4_data.js

--- views/view_4_projects/view_4_data.js
+++ views/view_4_projects/view_4_data.js
@@ -2,6 +2,7 @@
 FILE METADATA
 ================================================================
 FILE NAME    : view_4_data.js
+File Pach : FACILITYS_TRACKER_APP/views/view_4_projects/view_4_data.js
 SUPABASE TBL : facility_projects, project_actions, vendors, vendor_files, project_vendor_jobs, project_vendor_job_files, project_vendor_job_followups
 VIEW NAME    : Facility Projects Dashboard
 POP-UP TITLE : Create New Project / Add Project Action
