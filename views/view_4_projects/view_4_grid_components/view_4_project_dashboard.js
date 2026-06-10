354a1b516a3aefb1e723e474d01a83d17ff1bfeb
facilitytrackergms-hub <facilitytracker.gms@gmail.com>
2026-06-10 07:32:00 +0000

    Update view_4_project_dashboard.js

--- views/view_4_projects/view_4_grid_components/view_4_project_dashboard.js
+++ views/view_4_projects/view_4_grid_components/view_4_project_dashboard.js
@@ -18,17 +18,15 @@ import {
     setupProjectDashboardEvents
 } from '../view_4_modal.js';
 
-// =================== UPDATED IMPORT ===================
 import {
     escapeHtml,
     renderProjectActionModal,
     renderProjectActions
-} from './view_4_grid_components/view_4_render_helpers.js';
-// ======================================================
+} from './view_4_render_helpers.js';
 
 import {
     renderStyles
-} from './view_4_grid_components/view_4_styles.js';
+} from './view_4_styles.js';
 
 export async function renderSingleProjectDashboard({ facility, project }, nav) {
     const app = document.getElementById('app');
