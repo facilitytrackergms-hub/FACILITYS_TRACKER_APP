cf00c2308419211b9652968947a5710eeeb4c19e
facilitytrackergms-hub <facilitytracker.gms@gmail.com>
2026-06-12 12:07:53 -0400

    Update view_4_grid.js

--- views/view_4_projects/view_4_grid.js
+++ views/view_4_projects/view_4_grid.js
@@ -5,7 +5,7 @@ FILE NAME    : view_4_grid.js
 SUPABASE TBL : facility_projects, project_actions, vendors, vendor_files, project_vendor_jobs, project_vendor_job_files, project_vendor_job_followups
 VIEW NAME    : Facility Projects Dashboard
 POP-UP TITLE : Create New Project / Add Project Action
-LAST UPDATED : 2026-06-12 @ 08:30 AM
+LAST UPDATED : 2026-06-12 @ 12:06 PM
 ================================================================*/
 const __FILENAME = 'view_4_grid.js';
 
@@ -49,5 +49,5 @@ export async function renderVendorJobDashboard(data, nav) {
 
 /*================================================================
 END FILE: view_4_grid.js
-UPDATED: 2026-06-12 @ 08:30 AM
+UPDATED: 2026-06-12 @ 12:06 PM
 ================================================================*/
