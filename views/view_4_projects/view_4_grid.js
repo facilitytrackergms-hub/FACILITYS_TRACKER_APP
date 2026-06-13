183246f3a4af07e1da06c16871f28139fad63412
facilitytrackergms-hub <facilitytracker.gms@gmail.com>
2026-06-12 23:51:56 -0400

    Add renderProjectReportBuilderView to dashboards

--- views/view_4_projects/view_4_grid.js
+++ views/view_4_projects/view_4_grid.js
@@ -5,7 +5,7 @@ FILE NAME    : view_4_grid.js
 File Path    : FACILITYS_TRACKER_APP/views/view_4_projects/view_4_grid.js
 SUPABASE TBL : facility_projects, project_actions, vendors, vendor_files
 VIEW NAME    : Facility Projects Dashboard Entry Router
-LAST UPDATED : 2026-06-12 @ 04:22 PM
+LAST UPDATED : 2026-06-12 @ 11:55 PM
 ================================================================*/
 const __FILENAME = 'view_4_grid.js';
 
@@ -14,6 +14,7 @@ import { renderProjectsHome } from './view_4_grid_components/view_4_home.js';
 import { renderSingleProjectDashboard } from './view_4_grid_components/view_4_project_dashboard.js';
 import { renderSingleVendorDashboard } from './view_4_grid_components/view_4_vendor_dashboard.js';
 import { renderSingleVendorJobDashboard } from './view_4_grid_components/view_4_vendor_job_dashboard.js';
+import { renderProjectReportBuilderView } from './view_4_grid_components/view_4_report_builder.js';
 import { renderVendorQuotesFilesDashboard } from './view_4_grid_components/view_4_render_helpers.js';
 
 export async function renderPendingProjects(data, nav) {
@@ -21,29 +22,49 @@ export async function renderPendingProjects(data, nav) {
         renderPendingProjects,
         renderProjectDashboard,
         renderVendorDashboard,
-        renderVendorJobDashboard
+        renderVendorJobDashboard,
+        renderProjectReportBuilderView
     });
 }
 
 export async function renderProjectDashboard(data, nav) {
     return renderSingleProjectDashboard(data, nav || {
-        renderPendingProjects
+        renderPendingProjects,
+        renderProjectDashboard,
+        renderVendorDashboard,
+        renderVendorJobDashboard,
+        renderProjectReportBuilderView
     });
 }
 
 export async function renderVendorDashboard(data, nav) {
     return renderSingleVendorDashboard(data, nav || {
         renderPendingProjects,
-        renderVendorJobDashboard
+        renderProjectDashboard,
+        renderVendorJobDashboard,
+        renderProjectReportBuilderView
     });
 }
 
 export async function renderVendorJobDashboard(data, nav) {
     return renderSingleVendorJobDashboard(data, nav || {
-        renderVendorDashboard
+        renderVendorDashboard,
+        renderProjectDashboard,
+        renderProjectReportBuilderView
+    });
+}
+
+export async function renderReportBuilder(data, nav) {
+    return renderProjectReportBuilderView(data, nav || {
+        renderPendingProjects,
+        renderProjectDashboard,
+        renderVendorDashboard,
+        renderVendorJobDashboard,
+        renderProjectReportBuilderView
     });
 }
 
 /*================================================================
 END FILE: view_4_grid.js
+UPDATED: 2026-06-12 @ 11:55 PM
 ================================================================*/
